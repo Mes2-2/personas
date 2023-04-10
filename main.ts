5ab679acd26b41b77a114ddae110cf95ca1704b2
Mes2-2 <122781305+Mes2-2@users.noreply.github.com>
2023-04-10 09:49:16 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    persona += 1
+    basic.showNumber(persona)
+})
+input.onButtonPressed(Button.B, function () {
+    persona += -1
+    basic.showNumber(persona)
+})
+let persona = 0
+basic.showNumber(0)
+persona = 0
